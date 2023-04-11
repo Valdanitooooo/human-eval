8e5600f25ae9321a5dc46209566f6633368bcead
valdanito <valdanito@foxmail.com>
2023-04-11 18:01:19 +0800

    feat: add llama test

---
+++ llama_test_64.py
@@ -0,0 +1,24 @@
+import requests
+import json
+from human_eval.data import write_jsonl, read_problems
+
+
+url = "http://xxxx:8013/generate"
+
+def generate_llama_completion(p):
+    print(p)
+    params = {"max_length": 64, "prompt": p}
+    data = json.dumps(params)
+    print(data)
+    res = requests.post(url=url, data=data)
+    ret = res.json()
+    print(ret)
+    return ret.get('generated')[0]
+
+problems = read_problems()
+
+samples = []
+for task_id, task in problems.items():
+    samples.append(dict(task_id=task_id, completion=generate_llama_completion(task["prompt"])))
+
+write_jsonl("data/llama13B_64.jsonl", samples)
---
+++ test_result.md
@@ -0,0 +1,5 @@
+|   |  model | tokens |  HumanEval@1 |
+|---|---|---|---|
+|  OpenAI | text-davinci-003  | 64  |  32.93% |
+| LLaMA  |  13B |  64 | 3.66% |
+| LLaMA  |  13B |  128 | 6.10%  |
